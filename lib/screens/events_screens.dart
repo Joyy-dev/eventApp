44ab0c04d5d6656d84af43fdb1d218ab1ee9d1bd
joyy-dev <joyceava874@gmail.com>
2025-10-19 00:21:18 -0700

    final event screen

--- lib/screens/events_screens.dart
+++ lib/screens/events_screens.dart
@@ -1,6 +1,7 @@
 import 'package:eventease/widget/category_navigation.dart';
 import 'package:eventease/widget/custom_botton.dart';
 import 'package:eventease/widget/event_search.dart';
+import 'package:eventease/widget/nearby_events.dart';
 import 'package:eventease/widget/popular_events.dart';
 import 'package:flutter/material.dart';
 
@@ -27,79 +28,98 @@ class EventsScreens extends StatelessWidget {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: CustomBotton(),
 
-      body: Container(
-        color: const Color.fromARGB(255, 230, 230, 230),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 60, left: 20, right: 20),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Row(
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Find events near'),
-                              Row(
-                                children: [
-                                  Text(
-                                    'Jakarta, Indonesia', 
-                                    style: TextStyle(
-                                      fontSize: 25,
-                                      fontWeight: FontWeight.bold
-                                    ),),
-                                  Icon(Icons.arrow_drop_down_outlined, size: 30,)
-                                ],
-                              )
-                            ],
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      IconButton(
-                        onPressed: () {}, 
-                        icon: Icon(Icons.notifications_none_outlined)
-                      ),
-                      CircleAvatar(
-                        backgroundImage: AssetImage('assets/images/concert.png'),
-                      )
-                    ],
-                  )
-                ],
-              ),
-              SizedBox(height: 20,),
-              EventSearch(),
-              SizedBox(height: 20,),
-              Row(
-                children: [
-                  Text(
-                    'Popular event',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold
+      body: SingleChildScrollView(
+        child: Container(
+          color: const Color.fromARGB(255, 230, 230, 230),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 60, left: 20, right: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Find events near'),
+                                Row(
+                                  children: [
+                                    Text(
+                                      'Jakarta, Indonesia', 
+                                      style: TextStyle(
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.bold
+                                      ),),
+                                    Icon(Icons.arrow_drop_down_outlined, size: 30,)
+                                  ],
+                                )
+                              ],
+                            ),
+                          ],
+                        )
+                      ],
                     ),
-                  ),
-                  IconButton(
-                    onPressed: () {}, 
-                    icon: Icon(Icons.chevron_right)
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 290,
-                child: PopularEvents()
-              ),
-              CategoryNavigation()
-            ],
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {}, 
+                          icon: Icon(Icons.notifications_none_outlined)
+                        ),
+                        CircleAvatar(
+                          backgroundImage: AssetImage('assets/images/profile.png'),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(height: 20,),
+                EventSearch(),
+                SizedBox(height: 20,),
+                Row(
+                  children: [
+                    Text(
+                      'Popular event',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {}, 
+                      icon: Icon(Icons.chevron_right)
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 290,
+                  child: PopularEvents()
+                ),
+                CategoryNavigation(),
+                SizedBox(height: 10,),
+                Row(
+                  children: [
+                    Text(
+                      'Nearby Event Now',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {}, 
+                      icon: Icon(Icons.chevron_right_outlined)
+                    )
+                  ],
+                ),
+                NearbyEvents()
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/widget/nearby_events.dart
@@ -0,0 +1,127 @@
+import 'dart:async';
+import 'package:intl/intl.dart';
+
+import 'package:eventease/models/events.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class NearbyEvents extends StatefulWidget {
+  const NearbyEvents({super.key});
+
+  @override
+  State<NearbyEvents> createState() => _NearbyEventsState();
+}
+
+class _NearbyEventsState extends State<NearbyEvents> {
+  late final PageController _pageController;
+  Timer? _timer;
+
+  List<List<Events>> get nearbyEventGrid {
+    final event = Provider.of<allEvents>(context);
+    final nearEvent =  event.events;
+
+    List<List<Events>> eventGrid = [];
+    for (int i = 0; i < nearEvent.length; i += 3) {
+      eventGrid.add(nearEvent.skip(i).take(3).toList());
+    }
+    return eventGrid;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+    _timer = Timer.periodic(Duration(seconds: 2), (Timer) {
+    if (_pageController.hasClients) {
+      int nextPage = _pageController.page!.round() + 1;
+      _pageController.animateToPage(
+        nextPage, 
+        duration: Duration(milliseconds: 400), 
+        curve: Curves.easeInOut,
+      );
+    }
+  });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 230,
+      child: PageView.builder(
+        controller: _pageController,
+        itemCount: nearbyEventGrid.length * 1000,
+        itemBuilder: (context, index) {
+          final NearbyEvents = nearbyEventGrid[index % nearbyEventGrid.length];
+          return GridView.builder(
+            padding: EdgeInsets.zero,
+            itemCount: NearbyEvents.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+              childAspectRatio: 5 / 8
+            ), 
+            itemBuilder: (context, index) {
+              final event = NearbyEvents[index];
+      
+              return Container(
+                padding: EdgeInsets.all(8),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+                    image: AssetImage(event.eventimage),
+                    fit: BoxFit.cover
+                  )
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(''),
+                        CircleAvatar(
+                          backgroundColor: Colors.grey,
+                          foregroundColor: Colors.white,
+                          child: Icon(event.isFavorite ? Icons.favorite : Icons.favorite_border_outlined),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          event.eventtitle,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16
+                          ),
+                        ),
+                        Text(
+                          DateFormat('MMM d').format(event.eventdate),
+                          style: TextStyle(
+                            color: Colors.white
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }
+          );
+        },
+      ),
+    );
+  }
+}
